7d02927537627333414117d4632d2a6abcc4bb73
Kalpraj Anand <kalprajanand@gmail.com>
2024-01-27 01:31:45 +0530

    Data Transformation& modeling

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from src.MLprojects.logger import logging
 from src.MLprojects.exception import CustomException
 from src.MLprojects.components.data_ingestion import DataIngestion 
 from src.MLprojects.components.data_ingestion import DataIngestionConfig 
+from src.MLprojects.components.data_transformatiom import DataTransformationConfig,DataTransformation
+from src.MLprojects.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 import sys
 
@@ -13,7 +15,16 @@ if __name__=="__main__":
     try:
         ##data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+         #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        
+        
+        ## Model Training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
         
     except Exception as ex:
         logging.info("custom Exception")
